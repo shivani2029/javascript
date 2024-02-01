b01736c5733edb0cd08c123a69f4cf0be244c4dc
shivani2029 <helloshivi123@gmail.com>
2024-02-01 11:56:48 +0530

    first commit

---
+++ js/a.js
@@ -0,0 +1,12 @@
+ var a=12;
+ var a=120;
+ var a=110;
+
+ let b=10
+ //let b=100
+ b=100
+
+ const c= 100000
+ // const c=500000
+
+ document.write(a)
---
+++ js/set.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("a").innerHTML = "<center><h1>PNINFOSY GWALIOR</h1></center>";
+// document.getElementById("b").innerHTML = "<h1>Phone:7000846823</h1>"
+document.getElementById("a").setAttribute("style","border:10px dotted red");
+// document.getElementById("a").removeAttribute("style");
